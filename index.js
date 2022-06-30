5ba8e031bbc722d3325c0f31c52ac19ba6d73b8b
anhpeter <webfullstack99@gmail.com>
2022-06-30 15:08:33 +0700

    .

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const express = require("express");
 const mongoose = require("mongoose");
+const moment = require("moment");
 
 const app = express();
 app.use(express.json({ extended: false }));
@@ -10,33 +11,47 @@ mongoose
   .connect(
     "mongodb+srv://admin:VhN1XrRdTvwG1TJX@hobbies.ba9s98h.mongodb.net/kaffeine?retryWrites=true&w=majority"
   )
-  .catch((e) => console.error(err));
+  .catch((e) => console.error(e));
 
 const logSchema = new mongoose.Schema({
   type: String,
   message: String,
   timestamp: String,
 });
+const Log = mongoose.model("logs", logSchema);
 
-const LogModel = mongoose.model("logs", logSchema);
+// LOG MODEL
+const LogModel = {
+  saveExceptionLog: (message) => {
+    return new Log({ type: "exception", message }).save();
+  },
+  savePingLog: () => {
+    return new Log({
+      type: "ping",
+      timestamp: new Date().toISOString(),
+    }).save();
+  },
+  savePingMeLog: (message) => {
+    return new Log({
+      type: "ping-me",
+      message,
+      timestamp: new Date().toISOString(),
+    }).save();
+  },
+};
 
-const saveExceptionLog = (message) => {
-  return new LogModel({ type: "exception", message }).save();
-};
-const savePingLog = () => {
-  return new LogModel({
-    type: "ping",
-    timestamp: new Date().toISOString(),
-  }).save();
-};
-const savePingMeLog = (message) => {
-  return new LogModel({
-    type: "ping-me",
-    message,
-    timestamp: new Date().toISOString(),
-  }).save();
+const Helper = {
+  toReadableTime: (timestamp) => {
+    const diffMinutes = moment(new Date()).diff(timestamp, "minutes");
+    const result = `${diffMinutes} minute${diffMinutes > 1 ? "s" : ""} ago`;
+    return result;
+  },
 };
 
+// CONSTANTS
+const PORT = process.env.PORT || 3000;
+const pingUrlIn = 1 * 60 * 1000;
+const pingMeIn = 1 * 60 * 1000;
 const pitoBlogUrls = [
   "https://pitoghichep.com",
   "https://pitoblogapi.as.r.appspot.com",
@@ -45,34 +60,33 @@ const pitoBlogUrls = [
 const pingUrls = (urls) => {
   urls.forEach((url) =>
     axios.get(url).catch((e) => {
-      saveExceptionLog(e.message);
+      LogModel.saveExceptionLog(e.message);
       console.error(`failed to ping ${url}`);
     })
   );
 };
 
-// PING URLS
-let pingInterval = 1 * 60 * 1000;
+// PING URLS INTERVAL
 setInterval(async () => {
   pingUrls(pitoBlogUrls);
   //
-  savePingLog();
-}, [pingInterval]);
+  LogModel.savePingLog();
+}, [pingUrlIn]);
 
-// PING ME
+// PING ME INTERVAL
 const appUrl = process.env.PROJECT_DOMAIN
   ? `https://${process.env.PROJECT_DOMAIN}.glitch.me`
-  : "http://localhost:3000";
+  : `http://localhost:${PORT}`;
 setInterval(() => {
   axios
     .get(`${appUrl}/ping`)
     .then((res) => {
-      savePingMeLog(res.data);
+      LogModel.savePingMeLog(res.data);
     })
     .catch((e) => {
-      saveExceptionLog(e.message);
+      LogModel.saveExceptionLog(e.message);
     });
-}, 10000);
+}, pingMeIn);
 //
 app.get("/ping", (req, res) => {
   res.send("ping me!");
@@ -82,15 +96,15 @@ app.get("/ping", (req, res) => {
 app.get("/", async (req, res) => {
   const pingUrls = pitoBlogUrls;
   try {
-    const pings = await LogModel.find({ type: "ping" })
+    const pings = await Log.find({ type: "ping" })
       .sort({ timestamp: -1 })
       .limit(10);
     //
-    const pingMes = await LogModel.find({ type: "ping-me" })
+    const pingMes = await Log.find({ type: "ping-me" })
       .sort({ timestamp: -1 })
       .limit(10);
     //
-    const exceptions = await LogModel.find({ type: "exception" })
+    const exceptions = await Log.find({ type: "exception" })
       .sort({ timestamp: -1 })
       .limit(50);
     const exceptionMessages = exceptions.map((ex) => ({
@@ -103,16 +117,20 @@ app.get("/", async (req, res) => {
         {
           message: "kaffeine works",
           pingUrls,
-          pingHistory: pings.map((item) => item.timestamp),
+          pingHistory: pings.map((item) =>
+            Helper.toReadableTime(item.timestamp)
+          ),
           exceptions: exceptionMessages,
-          pingMeHistory: pingMes.map((item) => item.timestamp),
+          pingMeHistory: pingMes.map((item) =>
+            Helper.toReadableTime(item.timestamp)
+          ),
         },
         null,
         4
       )
     );
   } catch (e) {
-    saveExceptionLog(e.message);
+    LogModel.saveExceptionLog(e.message);
     res.status(500);
     res.json({ message: "failed to fetch ping history" });
   }
@@ -121,18 +139,17 @@ app.get("/", async (req, res) => {
 // RESET
 app.get("/reset", async (req, res) => {
   try {
-    const result = await LogModel.deleteMany();
+    const result = await Log.deleteMany();
     res.json(result);
   } catch (e) {
-    saveExceptionLog(e.message);
+    LogModel.saveExceptionLog(e.message);
     res.status(500);
     res.json({ message: "failed to reset" });
   }
 });
 
 //
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${appUrl}`);
+  console.info(`Server is running on port ${appUrl}`);
 });
 //
